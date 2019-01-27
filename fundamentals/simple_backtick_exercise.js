d6898e6c0a8ffce1aee293b7f50badd800654fd5
Antonio Savage <afmsavage@gmail.com>
2019-01-27 07:07:42 -0600

    Create simple_backtick_exercise.js
    https://javascript.info/types

---
+++ fundamentals/simple_backtick_exercise.js
@@ -0,0 +1,22 @@
+//What is the output of the script?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+
+
+//my answers
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
+
+//I WAS CORRECT!
