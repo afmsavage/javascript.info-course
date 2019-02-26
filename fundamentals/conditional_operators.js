a992cacf3e7dbf415c94c6daaf8c4fa0a591de95
Antonio Savage <asavage@oncourselearning.com>
2019-02-26 16:37:52 -0600

    Create conditional_opeartors.js lesson
    Created the conditional operators lesson.  Has to do with if and ? operators.  http://javascript.info/ifelse

---
+++ fundamentals/conditional_operators.js
@@ -0,0 +1,76 @@
+// Conditional Operators - if and '?'
+// http://javascript.info/ifelse
+
+// notes
+
+
+
+
+
+/*  TASK 1
+Will alert be shown?
+
+if ("0") {
+  alert( 'Hello' );
+}
+*/
+
+
+
+/*  TASK 2
+Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “Didn’t know? ECMAScript!”
+
+                         ---FLOWCHART FROM WEBSITE---
+                                    Begin
+                                      |
+                        "Whats the official name of Javascript"
+                         |                                  |
+                        Other                            ECMAScript
+                         |                                  |
+                    "You don't know?                    "RIGHT!"
+                      ECMASCRIPT!"
+*/
+
+
+
+/*  TASK 3
+Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+1, if the value is greater than zero,
+-1, if less than zero,
+0, if equals zero.
+In this task we assume that the input is always a number.
+*/
+
+
+
+/*  TASK 4
+Rewrite this if using the ternary operator '?':
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+*/
+
+
+
+/*  TASK 5
+Rewrite if..else using multiple ternary operators '?'.
+
+For readability, it’s recommended to split the code into multiple lines.
+
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+*/
