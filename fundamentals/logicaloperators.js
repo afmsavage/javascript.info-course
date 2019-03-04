d8324c1f6c2c9715013ac5f1759bbcb270812d0b
Antonio Savage <afmsavage@gmail.com>
2019-03-03 21:02:24 -0600

    Create logicaloperators.js
    https://javascript.info/logical-operators

---
+++ fundamentals/logicaloperators.js
@@ -0,0 +1,57 @@
+// Logical Opertors
+// https://javascript.info/logical-operators
+
+// notes
+
+
+
+
+
+
+
+
+
+
+/* Task 1
+Output of
+alert( null || 2 || undefined );
+
+
+*/
+
+/* Task 2
+Output of
+alert( alert(1) || 2 || alert(3) );
+*/
+
+/* Task 3
+What will this code show?
+alert( 1 && null && 2 );
+*/
+
+/* Task 4
+What will this code show?
+alert( alert(1) && alert(2) );
+*/
+
+/* Task 5
+What will the result be?
+alert( null || 2 && 3 || 4 );
+*/
+
+/* task 6
+Write an if condition to check that age is NOT between 14 and 90 inclusively.
+
+Create two variants: the first one using NOT !, the second one – without it.
+*/
+
+/* Task 6
+Which of these alerts are going to execute?
+
+What will the results of the expressions be inside if(...)?
+
+if (-1 || 0) alert( 'first' );
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );
+*/
+
