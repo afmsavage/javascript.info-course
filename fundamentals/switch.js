a6da57cd8d9dd94310840a75c581ae85ef9ce692
Antonio Savage <afmsavage@gmail.com>
2019-04-07 15:46:04 -0500

    updated switch.js

--- fundamentals/switch.js
+++ fundamentals/switch.js
@@ -1,3 +1,19 @@
 // switch statement
 // https://javascript.info/switch
 
+//syntax
+/*switch(x) {
+    case 'value1':  // if (x === 'value1')
+      ...
+      [break]
+  
+    case 'value2':  // if (x === 'value2')
+      ...
+      [break]
+  
+    default:
+      ...
+      [break]
+  } */
+
+  
